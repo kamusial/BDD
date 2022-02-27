a3746b32c0320c321c143163f52e2e529b9206b2
Musial <kamil.1.musial@nokia.com>
2022-02-27 12:27:26 +0100

    BDD1

--- the_numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-X = [i+1 for i in range(0,10)] # generowanie 10 punktów na osi Y
-Y = [5*i - 2 for i in X] # generowanie wartości na osi Y
-plt.plot(X,Y,'ro-')
-plt.show()
